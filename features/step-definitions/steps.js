5584fa0b80fb2a88ebab449bb259880ca46bf156
Ilich García <ilichgarcia27@gmail.com>
2022-10-15 12:56:49 -0600

    Testing regex

--- features/step-definitions/steps.js
+++ features/step-definitions/steps.js
@@ -96,7 +96,6 @@ Then(/^I shouldn't be able to see any book on my books collection$/, async () =>
 
 Then(/^I should get redirected to the login page$/, async () => {
     chaiExpect(await LoginPage.userName).to.exist;
-    chaiExpect(await LoginPage.userName).to.exist;
     chaiExpect(await LoginPage.password).to.exist;
     chaiExpect(await LoginPage.login).to.exist;
 });
